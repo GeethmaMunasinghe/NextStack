c1e376e25473827ed7296a8685328201fcbcf7e6
Geethma Munasinghe <geethma_2020698@fot.ruh.ac.lk>
2025-09-01 15:35:18 +0530

    ArrayList

---
+++ collection_framework/src/Demo8.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Demo8 {
+    public static void main(String[] args) {
+        ArrayList<String> list=new ArrayList();
+        list.add("Geethma");
+        list.add("Danidu");
+        list.add("Tharuka");
+
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println(list.get(0));
+
+        System.out.println(list.isEmpty());
+
+    }
+}
