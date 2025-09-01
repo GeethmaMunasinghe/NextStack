f4e85d19ff13e0f95b044b968fa879498a56f94d
Geethma Munasinghe <geethma_2020698@fot.ruh.ac.lk>
2025-09-01 13:22:58 +0530

    split

---
+++ collection_framework/src/Demo7.java
@@ -0,0 +1,8 @@
+public class Demo7 {
+    public static void main(String[] args) {
+        String name="My name is Geethma";
+
+        String [] arr=name.split(" ");
+        System.out.println(arr[3]);
+    }
+}
