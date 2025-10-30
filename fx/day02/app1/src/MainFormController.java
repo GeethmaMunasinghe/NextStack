f2a87e5b0009cdb52bad16c8fa26b09795d2dc7b
Geethma Munasinghe <geethma_2020698@fot.ruh.ac.lk>
2025-10-30 11:28:51 +0530

    textField and Buttons

--- fx/day02/app1/src/MainFormController.java
+++ fx/day02/app1/src/MainFormController.java
@@ -1,2 +1,11 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class MainFormController {
+    public TextField txtName;
+
+    public void printNameOnAction(ActionEvent actionEvent) {
+        String name=txtName.getText();
+        System.out.println(name);
+    }
 }
