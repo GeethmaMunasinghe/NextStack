61f28c74c527ad14ceccabbd47e3e2576001aa3d
Geethma Munasinghe <geethma_2020698@fot.ruh.ac.lk>
2025-10-30 11:47:25 +0530

    onKeyPressed

--- fx/day02/app1/src/MainFormController.java
+++ fx/day02/app1/src/MainFormController.java
@@ -1,5 +1,6 @@
 import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 public class MainFormController {
     public TextField txtName;
@@ -7,5 +8,9 @@ public class MainFormController {
     public void printNameOnAction(ActionEvent actionEvent) {
         String name=txtName.getText();
         System.out.println(name);
+    }
+
+    public void onKeyPressed(KeyEvent keyEvent) {
+        System.out.println(txtName.getText());
     }
 }
