688705ca20a8ec381f68af3333fa1ed86fd4f25b
Geethma Munasinghe <geethma_2020698@fot.ruh.ac.lk>
2025-10-31 14:00:00 +0530

    confirmation alert

--- fx/day03/app4/src/MainFormController.java
+++ fx/day03/app4/src/MainFormController.java
@@ -1,9 +1,9 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.Optional;
 
 public class MainFormController {
     public TableColumn<StudentTM,String> colNic;
@@ -12,23 +12,35 @@ public class MainFormController {
     public TableColumn<StudentTM,Button> colOption;
     public TableView<StudentTM> tblStudent;
 
-    public void initialize(){
+    public void initialize() {
         colNic.setCellValueFactory(new PropertyValueFactory<>("nic"));
         colName.setCellValueFactory(new PropertyValueFactory<>("name"));
         colAge.setCellValueFactory(new PropertyValueFactory<>("age"));
         colOption.setCellValueFactory(new PropertyValueFactory<>("btn"));
-        ObservableList<StudentTM> studentObList= FXCollections.observableArrayList();
-        for (Student st:Database.studentDb
-             ) {
-            Button btn=new Button("Delete");
-            studentObList.add(new StudentTM(
-                    st.getNic(),
-                    st.getName(),
-                    st.getAge(),
-                    btn
-            ));
-
-        }
-        tblStudent.setItems(studentObList);
+        loadTable();
     }
+        private void loadTable () {
+            ObservableList<StudentTM> studentObList = FXCollections.observableArrayList();
+            for (Student st : Database.studentDb
+            ) {
+                Button btn = new Button("Delete");
+                studentObList.add(new StudentTM(
+                        st.getNic(),
+                        st.getName(),
+                        st.getAge(),
+                        btn
+                ));
+                btn.setOnAction(event -> {
+                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure?", ButtonType.YES, ButtonType.NO);
+                    Optional<ButtonType> buttonType = alert.showAndWait(); //null point exception handle in Optional
+                    if (buttonType.get() == ButtonType.YES) {
+                        Database.studentDb.remove(st);
+                        loadTable();
+                        new Alert(Alert.AlertType.INFORMATION, "Student Deleted " + st.getName()).show();
+                    }
+                });
+            }
+            tblStudent.setItems(studentObList);
+        }
+
 }
